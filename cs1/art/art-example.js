44bfcb952468a62ce560874e0393f5c90dfd4e06
ahob85 <aaron@ahob85.com>
2017-04-07 10:46:13 -0700

    Add files via upload

--- cs1/art/art-example.js
+++ cs1/art/art-example.js
@@ -1,45 +1,69 @@
+//global variables
 var canvas = document.getElementById("myCanvas");
 var pen = canvas.getContext("2d");
+var w = canvas.width;
+var h = canvas.height;
 
-pen.beginPath();
-pen.moveTo(0, 0);
-pen.lineTo(canvas.width, canvas.height);
-pen.strokeStyle = "red";
-pen.stroke();
-
-pen.beginPath();
-pen.moveTo(canvas.width, 0);
-pen.lineTo(0, canvas.height);
-pen.strokeStyle = "blue";
-pen.stroke();
-
-pen.beginPath();
-pen.moveTo(0, canvas.height / 2);
-pen.lineTo(canvas.width, canvas.height / 2);
-pen.strokeStyle = "green";
-pen.stroke();
-
-pen.beginPath();
-pen.moveTo(canvas.width / 2, 0);
-pen.lineTo(canvas.width / 2, canvas.height);
-pen.strokeStyle = "purple";
-pen.stroke();
-
-pen.beginPath();
-pen.arc(canvas.width / 2, canvas.height / 2, canvas.width / 2, 0, 2 * Math.PI);
-pen.strokeStyle = "orange";
-pen.stroke();
-
-pen.font = "30px Arial";
-pen.fillStyle = "red";
-pen.textAlign = "center";
-pen.fillText("Hello World", canvas.width / 2, canvas.height / 2);
-pen.fillText("I'm a computer.", canvas.width / 2, (canvas.height / 2) + 30);
-
-for(i = 0; i < canvas.width / 4; i++){
+//functions
+function drawLine(startX, startY, endX, endY, color){
     pen.beginPath();
-    pen.moveTo(i * 10, 0);
-    pen.lineTo(i * 10, canvas.height);
-    pen.strokeStyle = "green";
+    pen.moveTo(startX, startY);
+    pen.lineTo(endX, endY);
+    pen.strokeStyle = color;
     pen.stroke();
+    pen.closePath();
 }
+
+function drawCircle(x, y, radius, color, filled){
+    pen.beginPath();
+    pen.arc(x, y, radius, 0, Math.PI * 2);
+    if(filled){
+        pen.fillStyle = color;
+        pen.fill();
+    }
+    else{
+        pen.strokeStyle = color;
+        pen.stroke();
+    }
+    pen.closePath();
+}
+
+function drawRect(x, y, width, height, color, filled){
+    pen.beginPath();
+    pen.rect(x, y, width, height);
+    if(filled){
+        pen.fillStyle = color;
+        pen.fill();
+    }
+    else{
+        pen.strokeStyle = color;
+        pen.stroke();
+    }
+    pen.closePath();
+}
+
+function run(){
+    drawLine(0, 0, w, h, "red");
+    drawLine(w, 0, 0, h, "blue");
+    drawLine(0, h / 2, w, h / 2, "green");
+    drawLine(w / 2, 0, w / 2, h, "purple");
+    drawCircle(w / 2, h / 2, w / 2, "orange", false);
+    drawRect(w / 2, h / 2, 40, 40, "blue", true);
+    //some extra stuff for fun
+    pen.font = "30px Arial";
+    pen.fillStyle = "red";
+    pen.textAlign = "center";
+    pen.fillText("Hello World", canvas.width / 2, canvas.height / 2);
+    pen.fillText("I'm a computer.", canvas.width / 2, (canvas.height / 2) + 30);
+    for(i = 0; i < canvas.width / 4; i++){
+        pen.beginPath();
+        pen.moveTo(i * 10, 0);
+        pen.lineTo(i * 10, canvas.height);
+        pen.strokeStyle = "green";
+        pen.stroke();
+    }
+}
+
+run();
+
+
--- cs1/art/art.js
+++ cs1/art/art.js
@@ -1,8 +1,47 @@
+//global variables
 var canvas = document.getElementById("myCanvas");
 var pen = canvas.getContext("2d");
+var w = canvas.width;
+var h = canvas.height;
 
-pen.beginPath();
+//draw a red line from top-left to bottom-right of canvas
+pen.beginPath(); //always start with this
 pen.moveTo(0, 0);
-pen.lineTo(canvas.width, canvas.height);
+pen.lineTo(w, h);
 pen.strokeStyle = "red";
 pen.stroke();
+pen.closePath(); //always end with this
+
+//draw an empty red circle
+pen.beginPath();
+pen.arc(100, 100, 50, 0, Math.PI * 2);
+pen.strokeStyle = "blue";
+pen.stroke();
+pen.closePath();
+
+//draw a green-filled rectangle
+pen.beginPath();
+pen.rect(50, 50, 40, 40);
+pen.fillStyle = "green";
+pen.fill();
+pen.closePath();
+
+//functions save you a lot of time! no need to repeat
+function drawLine(startX, startY, endX, endY, color){
+    
+}
+
+function drawCircle(x, y, radius, color, filled){
+
+}
+
+function drawRect(x, y, width, height, color, filled){
+
+}
+
+//call functions here to draw lines and shapes
+function run(){
+
+}
+
+run();
