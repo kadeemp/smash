c14ec3b2604b458768c5190b107e19be1ca45035
ahob85 <aaron@ahob85.com>
2017-04-21 16:02:15 -0700

    Update script.js

--- cs1/amazing/script.js
+++ cs1/amazing/script.js
@@ -148,7 +148,7 @@ function processDrawing(){
     var testX = Number(document.getElementById("x").value);
     var testY = Number(document.getElementById("y").value);
     if((textX < 0 || textX > 400) || (testY < 30 || testY > 400)){
-        alert("Please enter valid coordinates and measurements!");
+        alert("Please enter valid coordinates!");
     }
     else{
         if(canvas == null){
