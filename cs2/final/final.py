18a853089c9aa393eead9a2f7cb1316c6c33be10
ahob85 <aaron@ahob85.com>
2017-05-30 13:19:05 -0700

    Add files via upload

--- cs2/final/final.py
+++ cs2/final/final.py
@@ -1,8 +1,9 @@
+# coding: utf-8
 # Final Data Analysis Project Example
 # By: Lucy Hobson and Linus Hobson
 # SMASH CS2 20XX
 
-# import libraries
+# Import libraries
 import matplotlib.pyplot as plt
 import numpy as np
 import sqlite3 
@@ -10,7 +11,7 @@ import csv
 
 
 # Create/Connect to a SQL database, and assign a "cursor"
-conn = sqlite3.connect("pokebase.sqlite3")
+conn = sqlite3.connect("mydatabase.sqlite3")
 cur = conn.cursor()
 
 
@@ -37,7 +38,9 @@ file = open("Pokemon.csv")
 csv_file = csv.reader(file)
 for row in csv_file:
     cur.execute("INSERT INTO Pokedex VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
-                (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12]))
+                (row[0], row[1], row[2], row[3], row[4], row[5], row[6], 
+                 row[7], row[8], row[9], row[10], row[11], row[12]))
+    
 conn.commit()
 
 
@@ -58,13 +61,13 @@ def get_mean(nums):
         return round(total / len(nums), 2)
 
 
-# Bring table data into Python
+# Bring table data into program
 cur.execute("SELECT hp FROM Pokedex LIMIT 5")
 for row in cur:
     print(row)
 
 
-# More testing
+# Looks like SELECT returns tuples. Just as we said before, records are just tuples.
 cur.execute("SELECT hp FROM Pokedex WHERE name = 'Pikachu';")
 data = cur.fetchone()[0] 
 print(data)
@@ -80,7 +83,7 @@ hp_mean = get_mean(hp_data)
 print("HP mean = ", hp_mean)
 
 
-# Is average HP for non-Legendary Pokemon less than that for Legendary Pokemon?
+# I wonder... is the average HP for non-Legendary Pokemon less than that for Legendary Pokemon?
 cur.execute("SELECT hp FROM Pokedex WHERE Legendary = 0")
 nonlegendary_hp = []
 for row in cur:
@@ -93,6 +96,69 @@ print("Non-Legendary HP mean =", get_mean(nonlegendary_hp))
 print("Legendary HP mean =", get_mean(legendary_hp))
 
 
+# Make a bar chart
+nonlegendary_means = []
+legendary_means = []
+
+hp, attack, defense, spatk, spdef, speed = [], [], [], [], [], []
+cur.execute("SELECT hp, attack, defense, spatk, spdef, speed FROM Pokedex WHERE Legendary = 0")
+for row in cur:
+    hp.append(row[0])
+    attack.append(row[1])
+    defense.append(row[2])
+    spatk.append(row[3])
+    spdef.append(row[4])
+    speed.append(row[5])
+nonlegendary_means.extend((get_mean(hp), get_mean(attack), 
+                          get_mean(defense), get_mean(spatk), 
+                          get_mean(spdef), get_mean(speed)))
+
+
+hp, attack, defense, spatk, spdef, speed = [], [], [], [], [], []
+cur.execute("SELECT hp, attack, defense, spatk, spdef, speed FROM Pokedex WHERE Legendary = 1")
+for row in cur:
+    hp.append(row[0])
+    attack.append(row[1])
+    defense.append(row[2])
+    spatk.append(row[3])
+    spdef.append(row[4])
+    speed.append(row[5])
+legendary_means.extend((get_mean(hp), get_mean(attack), 
+                        get_mean(defense), get_mean(spatk), 
+                        get_mean(spdef), get_mean(speed)))
+
+
+print("Nonlegendary means:", nonlegendary_means)
+print("Legendary means:", legendary_means)
+
+
+bar_width = 0.35
+x_range = [0, 1, 2, 3, 4, 5]
+y_range = list(range(0, 150, 10))
+plt.rc("axes", axisbelow=True)
+
+
+plt.figure(1)
+plt.xticks(x_range,  ["HP", "Attack", "Defense", "Spatk", "Spdef", "Speed"])
+plt.yticks(y_range)
+plt.grid(axis="y")
+plt.title("Pokemon Stat Averages")
+
+
+nonlegendary_range = [x - bar_width / 2 for x in x_range]
+legendary_range = [x + bar_width / 2 for x in x_range]
+
+
+plt.bar(nonlegendary_range, nonlegendary_means, bar_width, color='blue', label='non-legendary')
+plt.bar(legendary_range, legendary_means, bar_width, color='red', label='legendary')
+
+
+plt.legend().draggable()
+
+
+plt.show()
+
+
 # Write and test another function
 def get_sd(nums):
     mean = get_mean(nums)
@@ -101,12 +167,13 @@ def get_sd(nums):
         total += (num - mean) ** 2
     variance = total / (len(nums) - 1)
     return round(variance ** 0.5, 2)
+
 hp_sd = get_sd(hp_data)
 print("HP standard deviation =", hp_sd)
 
 
 # Make a histogram
-plt.figure(3)
+plt.figure(2)
 plt.title("Distribution of Pokemon HP\nmean = {}, sd = {}".format(hp_mean, hp_sd))
 plt.xlabel("HP")
 plt.ylabel("Frequency")
@@ -118,13 +185,13 @@ plt.grid(axis="y")
 plt.show()
 
 
-# There are outliers toward the right, especially that one or two waaaaay out there.
+# This looks positively skewed
 print(max(hp_data))
 
 
-# Highest HP value is 255. Which Pokemon has 255 HP?
+# Which Pokemon has 255 base HP?
 cur.execute("SELECT name FROM Pokedex WHERE hp = 255")
-print(cur.fetchone()[0]) # Blissey!
+print(cur.fetchone()[0])
 
 
 # Make a scatter plot
@@ -133,7 +200,10 @@ d_data, s_data = [], []
 for row in cur:
     d_data.append(row[0])
     s_data.append(row[1])
-plt.figure(2)
+plt.figure(3)
+plt.xticks(list(range(0, 250, 20)))
+plt.yticks(list(range(0, 250, 20)))
+plt.grid(True)
 plt.title("Pokemon Defense vs Speed")
 plt.xlabel("Defense")
 plt.ylabel("Speed")
@@ -143,7 +213,7 @@ plt.show()
 
 # Find Pearson correlation coefficient (r) between two variables
 correlation = round(np.corrcoef(d_data, s_data)[0][1], 2)
-print("Pearson's r =", correlation)
+print("r =", correlation)
 
 
 # Make another scatter plot
@@ -151,15 +221,20 @@ a_data = []
 cur.execute("SELECT attack FROM Pokedex")
 for row in cur:
     a_data.append(row[0])
-plt.figure(3)
+plt.figure(4)
+plt.xticks(list(range(0, 250, 20)))
+plt.yticks(list(range(0, 250, 20)))
+plt.grid(True)
 plt.title("Pokemon HP vs Attack Power")
 plt.xlabel("HP")
 plt.ylabel("Attack Power")
 plt.scatter(hp_data, a_data)
 plt.show()
 
+
 correlation = round(np.corrcoef(hp_data, a_data)[0][1], 2)
-print("Pearson's r =", correlation)
+print("r =", correlation)
+
 
 # Create a model
 model = np.polyfit(hp_data, a_data, deg=1)
@@ -167,16 +242,19 @@ print(model)
 
 
 # Plot the model onto the data
-x_range = list(range(max(hp_data))) # Just creates a list of integers from 0 to highest HP value in dataset
-p = np.poly1d(model)                # the function p sets up the y = ax + b equation for the next step
-y_range = p(x_range)                # for every value in x_range, multiply by a and add b (from our model)
-plt.figure(4)
-plt.title("Pokemon HP vs Attack Power\nPearson's r = {}".format(correlation))
+x_range = list(range(max(hp_data))) 
+p = np.poly1d(model)               
+y_range = p(x_range)                
+plt.figure(5)
+plt.xticks(list(range(0, 250, 20)))
+plt.yticks(list(range(0, 250, 20)))
+plt.grid(True)
+plt.title("Pokemon HP vs Attack Power\nr = {}".format(correlation))
 plt.xlabel("HP")
 plt.ylabel("Attack Power")
 plt.scatter(hp_data, a_data)
-plt.plot(x_range, y_range, color="red") # here's where we draw the line (no pun intended)
+plt.plot(x_range, y_range, color="red") 
 plt.show()
 
-#close the connection to the database
+
 conn.close()
