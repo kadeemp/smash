fff90a448f9ddc58ec764f3872963ddc08f6db1b
ahob85 <aaron@ahob85.com>
2019-01-18 12:40:33 -0800

    updated IL zips, script allows non-financial aid to apply to IL

--- cs1/eligibility/script.js
+++ cs1/eligibility/script.js
@@ -38,11 +38,15 @@
   */
 
   function getDistances() {
+    let containsIL = false;
     for(let site in sites) {
       let found = false;
       for(let i = 0; i < sites[site].length && !found; i++) {
         if(sites[site][i].zip_code == zipCode) {
           let valid = {name: site, distance: sites[site][i].distance};
+          if(valid.name === "SMASH Illinois Tech") {
+              containsIL = true;
+          }
           if(validSites.length === 0) {
             validSites.push(valid);
           }
@@ -62,20 +66,28 @@
         }
       }
     }
-    if(validSites.length === 0) {
-      message += "<p>Sorry, your zip code is not within 50 miles of a SMASH site.</p>";
-    }
-    else {
-      for(let i = 0; i < validSites.length; i++) {
-        message += "<p>You can apply to <b>" + validSites[i].name + " </b>(" + (Math.round(validSites[i].distance * 10) / 10) + " mi)</p>";
-      }
-      if(validSites.length === 1) {
-        message += "<p>Please be sure to select this site in your application.</p>";
-      }
-      else {
-        message += "<p>Please be sure to select <strong>ONE</strong> of the above sites in your application.</p>";
-      }
-      message += "<p><a href=\"https://app.smarterselect.com/programs/53973-Smash\">Click here to apply!</a></p>";
+    // rewrote this part to account for students applying to SMASH Illinois Tech
+    if(!grade || (!school && !containsIL) || validSites.length === 0) {
+        if(!grade) {
+          message += "<p>Sorry, only 9th graders are eligible to apply.</p>";
+        }
+        if(!school && !containsIL) {
+          message += "<p>Sorry, only students attending public schools, or who receive financial assistance at private schools, are eligible to apply.</p>";
+        }
+        if(validSites.length === 0) {
+          message += "<p>Sorry, your zip code is not within 50 miles of a SMASH site.</p>";
+        }
+    } else {
+        for(let i = 0; i < validSites.length; i++) {
+            message += "<p>You can apply to <b>" + validSites[i].name + " </b>(" + (Math.round(validSites[i].distance * 10) / 10) + " mi)</p>";
+        }
+        if(validSites.length === 1) {
+            message += "<p>Please be sure to select this site in your application.</p>";
+        }
+        else {
+            message += "<p>Please be sure to select <strong>ONE</strong> of the above sites in your application.</p>";
+        }
+        message += "<p><a href=\"https://app.smarterselect.com/programs/53973-Smash\">Click here to apply!</a></p>";
     }
     window.scrollTo(0, 0);
     document.getElementById("message-area").innerHTML = message;
@@ -93,14 +105,7 @@
     getFormData();
     document.getElementById("message-area").innerHTML = "Loading...";
     let zipCodeOkay = zipCode.length === 5 && !isNaN(Number(zipCode));
-    if(!school || !grade || !zipCodeOkay) {
-      if(!school) {
-        message += "<p>Sorry, only students attending public schools, or who receive financial assistance at private schools, are eligible to apply.</p>";
-      }
-      if(!grade) {
-        message += "<p>Sorry, only 9th graders are eligible to apply.</p>";
-      }
-      if(!zipCodeOkay) {
+    if(!zipCodeOkay) {
         message += "<p>Please enter zip code as 5 numbers: #####</p>";
       }
       document.getElementById("message-area").innerHTML = message;
